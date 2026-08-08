0559adee14714ee13d54dc74efd322a5b2f6b67b
yorky02 <vieiraoctavio_@outlook.com>
2026-08-08 01:19:32 -0700

    Configure PostgreSQL and fix listing admin

--- styleleveling/catalog/admin.py
+++ styleleveling/catalog/admin.py
@@ -7,7 +7,7 @@ class ListingAdmin(admin.ModelAdmin):
     list_display = ['store', 'current_price', 'original_price', 'discount_percentage', 'is_promo_active' , 'last_checked_time']
     list_filter = ['store__store_name', 'product__brand_name', 'is_promo_active']
     search_fields = ['product__product_name', 'external_product_id', 'product__brand_name', 'store__store_name']
-    readonly_fields = ['discount_percentage', 'last_checked_time', 'last_seen', 'product']
+    readonly_fields = ['discount_percentage', 'last_checked_time', 'last_seen',]
 
 admin.site.register(Store)
 admin.site.register(Product)
--- styleleveling/styleleveling/settings.py
+++ styleleveling/styleleveling/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
@@ -76,9 +78,13 @@ WSGI_APPLICATION = 'styleleveling.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
